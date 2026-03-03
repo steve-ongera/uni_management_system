7480b8a29c219d970b44090b53de61835827dcb3
steve-ongera <gadafisteve001@gmail.com>
2026-03-03 11:20:09 +0300

    made updates

--- backend/ict_admin/admin.py
+++ backend/ict_admin/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import SystemLog
 
-# Register your models here.
+
+@admin.register(SystemLog)
+class SystemLogAdmin(admin.ModelAdmin):
+    list_display = ['performed_by', 'action', 'model_name', 'object_id', 'description', 'timestamp']
+    list_filter = ['action', 'model_name']
+    search_fields = ['description', 'performed_by__username']
+    ordering = ['-timestamp']
+    readonly_fields = ['performed_by', 'action', 'model_name', 'object_id', 'description', 'timestamp']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
